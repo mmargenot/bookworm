276e6ee299f5b1e20ff5084f84b7a5b30330c750
Maxwell Margenot <max.margenot@gmail.com>
2026-03-29 17:24:16 -0400

    Add bookworm client for talking to remote services

--- src/bookworm/services/embedding_service.py
+++ src/bookworm/services/embedding_service.py
@@ -1,4 +1,9 @@
+import logging
+
 from bookworm.base import Embedder
+
+
+logger = logging.getLogger(__name__)
 
 
 class EmbeddingService:
@@ -6,5 +11,9 @@ class EmbeddingService:
         self.embedder = embedder
 
     def embed(self, texts: list[str]) -> list[list[float]]:
+        num_texts = len(texts)
+        logger.info(f"Embedding {num_texts} texts")
         embeddings = self.embedder.embed(texts)
+        embed_dim = len(embeddings[0])
+        logger.info(f"Built {num_texts} embeddings of dim {embed_dim}")
         return embeddings
